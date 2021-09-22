582955437eb7f2b77a1b33866753d642e7d991b2
unknown <karishma.kulkarni@vinsys.com>
2021-09-22 16:41:49 +0530

    commit

---
+++ src/test/java/selenium_practice/maven_git/Test.java
@@ -0,0 +1,13 @@
+package selenium_practice.maven_git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hi");
+
+	}
+
+}
